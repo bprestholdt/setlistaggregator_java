377ec0cd3af5527349ad94784047f2c3bef21f0a
Braden <braden.prestholdt@uwrf.edu>
2024-10-15 03:19:27 -0500

    fix structure

---
+++ src/main/java/com/example/setlistaggregatorjava/setlistaggregator/SetlistaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.setlistaggregatorjava.setlistaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/setlistaggregatorjava/setlistaggregator/SetlistaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setlistaggregatorjava.setlistaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
